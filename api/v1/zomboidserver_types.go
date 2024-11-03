a6b08a8e23c72b0215a0925b6cfe178c39b1eeac
Chris Guidry <chris@theguidrys.us>
2024-11-02 21:25:02 -0400

    Bootstraps the operator with `kubebuilder`, basic PVC and deployment setup

---
+++ api/v1/zomboidserver_types.go
@@ -0,0 +1,71 @@
+package v1
+
+import (
+	corev1 "k8s.io/api/core/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+)
+
+// ZomboidServerSpec defines the desired state of ZomboidServer.
+type ZomboidServerSpec struct {
+	// Version is the version of the Zomboid server to run.
+	Version string `json:"version"`
+
+	// Resources defines the compute resources required by the Zomboid server.
+	Resources corev1.ResourceRequirements `json:"resources"`
+
+	// Storage defines the persistent storage configuration for the Zomboid server.
+	Storage Storage `json:"storage"`
+
+	// Administrator defines the admin user credentials for the Zomboid server.
+	Administrator Administrator `json:"administrator"`
+}
+
+// Storage defines the persistent storage configuration for the Zomboid server.
+type Storage struct {
+	// StorageClassName is the name of the storage class to use for the PVC, if not set, the default storage class for the clusterwill be used.
+	// +optional
+	StorageClassName *string `json:"storageClassName,omitempty"`
+
+	// Resources defines the storage resources required by the Zomboid server.
+	Resources corev1.VolumeResourceRequirements `json:"resources"`
+}
+
+// Administrator defines the credentials for the admin user.
+type Administrator struct {
+	// Username is the admin username.
+	Username string `json:"username"`
+
+	// Password is a reference to a secret key containing the admin password.
+	Password corev1.SecretKeySelector `json:"password"`
+}
+
+// ZomboidServerStatus defines the observed state of ZomboidServer.
+type ZomboidServerStatus struct {
+	// INSERT ADDITIONAL STATUS FIELD - define observed state of cluster
+	// Important: Run "make" to regenerate code after modifying this file
+}
+
+// +kubebuilder:object:root=true
+// +kubebuilder:subresource:status
+
+// ZomboidServer is the Schema for the zomboidservers API.
+type ZomboidServer struct {
+	metav1.TypeMeta   `json:",inline"`
+	metav1.ObjectMeta `json:"metadata,omitempty"`
+
+	Spec   ZomboidServerSpec   `json:"spec,omitempty"`
+	Status ZomboidServerStatus `json:"status,omitempty"`
+}
+
+// +kubebuilder:object:root=true
+
+// ZomboidServerList contains a list of ZomboidServer.
+type ZomboidServerList struct {
+	metav1.TypeMeta `json:",inline"`
+	metav1.ListMeta `json:"metadata,omitempty"`
+	Items           []ZomboidServer `json:"items"`
+}
+
+func init() {
+	SchemeBuilder.Register(&ZomboidServer{}, &ZomboidServerList{})
+}
---
+++ internal/controller/zomboidserver_controller.go
@@ -0,0 +1,154 @@
+package controller
+
+import (
+	"context"
+	"fmt"
+
+	"k8s.io/apimachinery/pkg/runtime"
+	"k8s.io/utils/ptr"
+	ctrl "sigs.k8s.io/controller-runtime"
+	"sigs.k8s.io/controller-runtime/pkg/client"
+	"sigs.k8s.io/controller-runtime/pkg/controller/controllerutil"
+	"sigs.k8s.io/controller-runtime/pkg/log"
+
+	appsv1 "k8s.io/api/apps/v1"
+	corev1 "k8s.io/api/core/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+
+	zomboidv1 "github.com/hordehost/zomboid-operator/api/v1"
+)
+
+// ZomboidServerReconciler reconciles a ZomboidServer object
+type ZomboidServerReconciler struct {
+	client.Client
+	Scheme *runtime.Scheme
+}
+
+// +kubebuilder:rbac:groups=horde.host,resources=zomboidservers,verbs=get;list;watch;create;update;patch;delete
+// +kubebuilder:rbac:groups=horde.host,resources=zomboidservers/status,verbs=get;update;patch
+// +kubebuilder:rbac:groups=horde.host,resources=zomboidservers/finalizers,verbs=update
+
+// Reconcile is the main function that reconciles a ZomboidServer resource
+func (r *ZomboidServerReconciler) Reconcile(ctx context.Context, req ctrl.Request) (ctrl.Result, error) {
+	_ = log.FromContext(ctx)
+
+	zomboidServer := &zomboidv1.ZomboidServer{}
+	err := r.Get(ctx, req.NamespacedName, zomboidServer)
+	if err != nil {
+		return ctrl.Result{}, client.IgnoreNotFound(err)
+	}
+
+	deployment := &appsv1.Deployment{
+		ObjectMeta: metav1.ObjectMeta{
+			Name:      zomboidServer.Name,
+			Namespace: zomboidServer.Namespace,
+		},
+	}
+
+	pvc := &corev1.PersistentVolumeClaim{
+		ObjectMeta: metav1.ObjectMeta{
+			Name:      zomboidServer.Name + "-game-data",
+			Namespace: zomboidServer.Namespace,
+		},
+	}
+	_, err = controllerutil.CreateOrUpdate(ctx, r.Client, pvc, func() error {
+		if pvc.CreationTimestamp.IsZero() {
+			pvc.Spec = corev1.PersistentVolumeClaimSpec{
+				AccessModes: []corev1.PersistentVolumeAccessMode{
+					corev1.ReadWriteOnce,
+				},
+				StorageClassName: zomboidServer.Spec.Storage.StorageClassName,
+				Resources:        zomboidServer.Spec.Storage.Resources,
+			}
+		} else {
+			pvc.Spec.Resources = zomboidServer.Spec.Storage.Resources
+		}
+
+		return ctrl.SetControllerReference(zomboidServer, pvc, r.Scheme)
+	})
+	if err != nil {
+		return ctrl.Result{}, err
+	}
+
+	_, err = controllerutil.CreateOrUpdate(ctx, r.Client, deployment, func() error {
+		deployment.Spec = appsv1.DeploymentSpec{
+			Replicas: ptr.To(int32(1)),
+			Strategy: appsv1.DeploymentStrategy{
+				Type: appsv1.RecreateDeploymentStrategyType,
+			},
+			Selector: &metav1.LabelSelector{
+				MatchLabels: map[string]string{
+					"app": zomboidServer.Name,
+				},
+			},
+			Template: corev1.PodTemplateSpec{
+				ObjectMeta: metav1.ObjectMeta{
+					Labels: map[string]string{
+						"app": zomboidServer.Name,
+					},
+				},
+				Spec: corev1.PodSpec{
+					Containers: []corev1.Container{
+						{
+							Name:            "zomboid",
+							Image:           fmt.Sprintf("hordehost/zomboid-server:%s", zomboidServer.Spec.Version),
+							ImagePullPolicy: corev1.PullIfNotPresent,
+							Resources:       zomboidServer.Spec.Resources,
+							Env: []corev1.EnvVar{
+								{
+									Name:  "ZOMBOID_JVM_MAX_HEAP",
+									Value: fmt.Sprintf("%dm", zomboidServer.Spec.Resources.Limits.Memory().Value()/(1024*1024)),
+								},
+								{
+									Name:  "ZOMBOID_SERVER_NAME",
+									Value: zomboidServer.Name,
+								},
+								{
+									Name:  "ZOMBOID_SERVER_ADMIN_USERNAME",
+									Value: zomboidServer.Spec.Administrator.Username,
+								},
+								{
+									Name: "ZOMBOID_SERVER_ADMIN_PASSWORD",
+									ValueFrom: &corev1.EnvVarSource{
+										SecretKeyRef: &zomboidServer.Spec.Administrator.Password,
+									},
+								},
+							},
+							VolumeMounts: []corev1.VolumeMount{
+								{
+									Name:      "game-data",
+									MountPath: "/game-data",
+								},
+							},
+						},
+					},
+					Volumes: []corev1.Volume{
+						{
+							Name: "game-data",
+							VolumeSource: corev1.VolumeSource{
+								PersistentVolumeClaim: &corev1.PersistentVolumeClaimVolumeSource{
+									ClaimName: zomboidServer.Name + "-game-data",
+								},
+							},
+						},
+					},
+				},
+			},
+		}
+
+		return ctrl.SetControllerReference(zomboidServer, deployment, r.Scheme)
+	})
+	if err != nil {
+		return ctrl.Result{}, err
+	}
+
+	return ctrl.Result{}, nil
+}
+
+// SetupWithManager sets up the controller with the Manager.
+func (r *ZomboidServerReconciler) SetupWithManager(mgr ctrl.Manager) error {
+	return ctrl.NewControllerManagedBy(mgr).
+		For(&zomboidv1.ZomboidServer{}).
+		Named("zomboidserver").
+		Complete(r)
+}
